cb77065d2cbca9c762d531a412e409e47f5fa221
Seiya Sasaki <sirukufarios@gmail.com>
2020-01-26 18:31:08 +0900

    implement minimum API

--- scale.go
+++ scale.go
@@ -1,6 +1,24 @@
 package main
-import ("fmt")
+import (
+  // "fmt"
+  "github.com/gin-gonic/gin"
+)
 
-func main(){
-    fmt.Println("Hello, world!");
+// func main(){
+//     fmt.Println("Hello, world!");
+// }
+
+func main() {
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  r.GET("/hello", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "World !",
+    })
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080
 }
